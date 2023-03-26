1203bbe49fabb64c2abcec8c6f2f5f333f7f7a42
papersaltserver <noconnect@gmail.com>
2023-03-26 15:42:40 +0200

    fix: make use of single prisma instance

--- astro-pl-tracker/src/utils/contryRoutes.ts
+++ astro-pl-tracker/src/utils/contryRoutes.ts
@@ -2,42 +2,52 @@ export const countries = [
   {
     code: "by",
     path: "/by",
+    countryPath: true,
   },
   {
     code: "ua",
     path: "/ua",
+    countryPath: true,
   },
   {
     code: "de",
     path: "/de",
+    countryPath: true,
   },
   {
     code: "vn",
     path: "/vn",
+    countryPath: true,
   },
   {
     code: "in",
     path: "/in",
+    countryPath: true,
   },
   {
     code: "it",
     path: "/it",
+    countryPath: true,
   },
   {
     code: "ge",
     path: "/ge",
+    countryPath: true,
   },
   {
     code: "cn",
     path: "/cn",
+    countryPath: true,
   },
   {
     code: "uk",
     path: "/uk",
+    countryPath: true,
   },
   {
     code: "ru",
     path: "",
+    countryPath: false,
   },
 ];
 interface CountryParams {
@@ -46,5 +56,5 @@ interface CountryParams {
   };
 }
 export function getCountryRoutes(): CountryParams[] {
-  return countries.map((c) => ({ params: { country: c.code } }));
+  return countries.filter(c => c.countryPath).map((c) => ({ params: { country: c.code } }));
 }
