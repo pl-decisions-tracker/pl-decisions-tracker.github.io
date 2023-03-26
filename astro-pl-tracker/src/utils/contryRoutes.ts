cdb095cec73e199bbe9e5f5ced7037ad9c127bac
papersaltserver <noconnect@gmail.com>
2023-03-26 09:32:07 +0200

    feat: add menu to chose country

--- astro-pl-tracker/src/utils/contryRoutes.ts
+++ astro-pl-tracker/src/utils/contryRoutes.ts
@@ -1,54 +1,50 @@
+export const countries = [
+  {
+    code: "by",
+    path: "/by",
+  },
+  {
+    code: "ua",
+    path: "/ua",
+  },
+  {
+    code: "de",
+    path: "/de",
+  },
+  {
+    code: "vn",
+    path: "/vn",
+  },
+  {
+    code: "in",
+    path: "/in",
+  },
+  {
+    code: "it",
+    path: "/it",
+  },
+  {
+    code: "ge",
+    path: "/ge",
+  },
+  {
+    code: "cn",
+    path: "/cn",
+  },
+  {
+    code: "uk",
+    path: "/uk",
+  },
+  {
+    code: "ru",
+    path: "",
+  },
+];
 interface CountryParams {
-    params: {
-      country: string;
-    }
+  params: {
+    country: string;
+  };
 }
 export function getCountryRoutes(): CountryParams[] {
-  return [
-    {
-      params: {
-        country: "by"
-      }
-    },
-    {
-      params: {
-        country: "ua"
-      }
-    },
-    {
-      params: {
-        country: "de"
-      }
-    },
-    {
-      params: {
-        country: "vn"
-      }
-    },
-    {
-      params: {
-        country: "in"
-      }
-    },
-    {
-      params: {
-        country: "it"
-      }
-    },
-    {
-      params: {
-        country: "ge"
-      }
-    },
-    {
-      params: {
-        country: "cn"
-      }
-    },
-    {
-      params: {
-        country: "uk"
-      }
-    }
-  ];
+  return countries.map((c) => ({ params: { country: c.code } }));
 }
